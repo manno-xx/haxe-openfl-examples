584cf84d5e0d1412e3eea64338fd4d45474c2e3c
Manno <manno@xs4all.nl>
2016-09-08 11:03:44 +0200

    Updated Singleton demo to match the Haxe cookbook example (again ;) )

--- Singleton/Source/SingletonDemo.hx
+++ Singleton/Source/SingletonDemo.hx
@@ -9,7 +9,8 @@ class SingletonDemo
 {
 	// this static variable will store a reference to an instance of this class.
 	// because it is a static variable it's value is the same across all possible instances of the class
-	public static var instance(get, null):SingletonDemo;
+	// It is a class variable, not an instance variable
+	public static var instance(default, null):SingletonDemo = new SingletonDemo();
 
 	// this variable is not Singleton specific. It gives this singleton puropose: This singleton is a score tracker
 	private var score:Int = 0;
@@ -24,7 +25,7 @@ class SingletonDemo
 	 * 
 	 * That way this class can make sure there can be only one instance of this class:
 	 * - This class is the only one that can instantiate this class
-	 * - It will only do so if the variable in which an instance is stored is still empty
+	 * - It does so when the application starts and all static variables are initialized
 	 *
 	 * Other classes that want to use it must request access to the instance through the getInstance function
 	 * 
@@ -47,19 +48,5 @@ class SingletonDemo
 	public function addToScore( amount:Int )
 	{
 		score += amount;
-	}
-
-	/**
-	 * This static function, when called, will check if the static var instance already has a value
-	 * if not, it will be made to store a new instance of this class.
-	 *
-	 * In any case, the variable's value is returned; An instance of this class
-	 */
-	static function get_instance():SingletonDemo
-	{
-		if( instance == null )
-			instance = new SingletonDemo();
-
-		return instance;
 	}
 }
